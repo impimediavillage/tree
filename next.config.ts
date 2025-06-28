02161da6c05940dc63f79709c46a47e33b74fa59
Richard Vostradovsky <impimediavillage@gmail.com>
2025-06-28 05:43:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.leafly.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
