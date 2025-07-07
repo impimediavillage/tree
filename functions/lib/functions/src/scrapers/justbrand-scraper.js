5cd19d75fa8281df142fea76add038b7729704a7
Richard Vostradovsky <impimediavillage@gmail.com>
2025-07-07 11:34:38 +0000

    firebase deploy --only functions
    === Deploying to 'dispensary-tree'...

--- functions/lib/functions/src/scrapers/justbrand-scraper.js
+++ functions/lib/functions/src/scrapers/justbrand-scraper.js
@@ -1,44 +1,11 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runScraper = void 0;
 const axios_1 = __importDefault(require("axios"));
-const cheerio = __importStar(require("cheerio"));
+const cheerio_1 = __importDefault(require("cheerio"));
 const BASE_URL = 'https://www.justbrand.co.za';
 // --- UTILITY FUNCTIONS ---
 const slugify = (text) => {
@@ -104,7 +71,7 @@ const scrapeProductDetails = async (productHandle) => {
             title: productData.title,
             handle: productData.handle,
             productUrl,
-            description: cheerio.load(productData.body_html || '').text().trim(),
+            description: cheerio_1.default.load(productData.body_html || '').text().trim(),
             price: normalizePrice(productData.variants[0]?.price || '0'),
             priceMin: normalizePrice(productData.price_min?.toString() || '0'),
             priceMax: normalizePrice(productData.price_max?.toString() || '0'),
@@ -124,7 +91,7 @@ const scrapeProductHandlesForCategory = async (categoryUrl, log) => {
         log(`Scraping handles from category page: ${currentPageUrl}`);
         try {
             const { data } = await axiosWithRetry(currentPageUrl);
-            const $ = cheerio.load(data);
+            const $ = cheerio_1.default.load(data);
             $('a.product-item__image-wrapper').each((_, el) => {
                 const href = $(el).attr('href');
                 if (href) {
@@ -148,7 +115,7 @@ const scrapeCategories = async (log) => {
     log('Starting to scrape categories from /collections...');
     try {
         const { data } = await axiosWithRetry(`${BASE_URL}/collections`);
-        const $ = cheerio.load(data);
+        const $ = cheerio_1.default.load(data);
         const categories = [];
         $('a.collection-grid-item').each((_, el) => {
             const url = $(el).attr('href');
