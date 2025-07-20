839aae106fcc8591974c820dabdf96c742240147
Richard Vostradovsky <impimediavillage@gmail.com>
2025-07-20 11:11:38 +0000

    For public users submitting a new wellness store application form should

---
+++ src/next.config.ts
@@ -0,0 +1,56 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.leafly.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+        "https://6000-firebase-studio-1748549332563.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev"
+        // You can add more origins here if needed, e.g., other preview environments
+    ],
+  },
+   async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
+
+export default nextConfig;
