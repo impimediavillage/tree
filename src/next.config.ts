dfa04daf00890cd2275ebaf6901217b1388e1b09
Richard Vostradovsky <impimediavillage@gmail.com>
2025-07-20 13:24:50 +0000

    This happened after 839aae1 and changes to src/next.config.ts. I cant lo

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,4 +1,3 @@
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -36,20 +35,6 @@ const nextConfig: NextConfig = {
         "https://6000-firebase-studio-1748549332563.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev"
         // You can add more origins here if needed, e.g., other preview environments
     ],
-  },
-   async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 };
 
