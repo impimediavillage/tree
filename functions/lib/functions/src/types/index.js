b0dd1cbaa8bdfc6786e145e272210118451f9fb2
Richard Vostradovsky <impimediavillage@gmail.com>
2025-08-08 17:59:11 +0000

    Calling getUserProfile function... signin:1 Access to fetch at 'https://

--- functions/lib/functions/src/types/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-'use server';
-// This file is intentionally left blank.
-// All type definitions have been consolidated into `functions/src/types.ts`
-// to resolve TypeScript compilation issues.
-//# sourceMappingURL=index.js.map
