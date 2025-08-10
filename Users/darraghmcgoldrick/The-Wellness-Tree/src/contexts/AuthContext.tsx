08d223523b457e87ed847582887947f69a142a84
Richard Vostradovsky <impimediavillage@gmail.com>
2025-08-10 18:35:46 +0000

    AuthContext: Fetching user profile for UID: sQWL7iHBP0aJXM0Ko5MvOoGIx6F3

--- Users/darraghmcgoldrick/The-Wellness-Tree/src/contexts/AuthContext.tsx
+++ Users/darraghmcgoldrick/The-Wellness-Tree/src/contexts/AuthContext.tsx
@@ -6,7 +6,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import type { ReactNode } from 'react';
 import { createContext, useContext, useEffect, useState, useCallback } from 'react';
 import { auth, functions } from '@/lib/firebase';
-import type { User as AppUser, Dispensary } from '@/types';
+import type { User as AppUser, Dispensary } from 'functions/src/types';
 import { useRouter } from 'next/navigation';
 import { useToast } from '@/hooks/use-toast';
 import { httpsCallable, FunctionsError } from 'firebase/functions';
