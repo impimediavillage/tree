775d9c584fc25d49554cd9d4b069cff77bc7e2b8
Richard Vostradovsky <impimediavillage@gmail.com>
2025-08-12 13:30:46 +0000

    why are you stuck saying you are implmenting the full restorationof this

--- Users/darraghmcgoldrick/The-Wellness-Tree/src/types/index.ts
+++ Users/darraghmcgoldrick/The-Wellness-Tree/src/types/index.ts
@@ -1,8 +1,2 @@
 
-
-// All type definitions are now consolidated in functions/src/types.ts
-// This file is being cleared to avoid confusion and conflicts.
-
 export type * from 'functions/src/types';
-
-    
