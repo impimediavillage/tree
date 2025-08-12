ecfe71028cbc49c5024e068ca92599f105ee1dab
Richard Vostradovsky <impimediavillage@gmail.com>
2025-08-12 13:12:01 +0000

    what  the fuck is going on. I dotn want oacther or re vamps I want you t

--- Users/darraghmcgoldrick/The-Wellness-Tree/functions/src/index.ts
+++ Users/darraghmcgoldrick/The-Wellness-Tree/functions/src/index.ts
@@ -212,5 +212,3 @@ export const deductCreditsAndLogInteraction = onCall(async (request: CallableReq
         throw new HttpsError('internal', 'An internal error occurred while processing the transaction.');
     }
 });
-
-    
