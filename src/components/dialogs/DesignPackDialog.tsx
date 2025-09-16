2f1725ddd4b0aea949c810015496db1beacf24d4
Richard Vostradovsky <impimediavillage@gmail.com>
2025-09-16 10:51:40 +0000

    In Create Your Triple S Canna Club Pack the "Welcome to the Triple S Can

--- src/components/dialogs/DesignPackDialog.tsx
+++ src/components/dialogs/DesignPackDialog.tsx
@@ -146,7 +146,7 @@ export const DesignPackDialog: React.FC<DesignPackDialogProps> = ({ isOpen, onOp
                                     Based on the price of **ZAR {tier?.price.toFixed(2)}**, you can select **{maxSelectable}** sticker(s) from our collection to bundle with your unique AI-generated design.
                                 </DialogDescription>
                             </DialogHeader>
-                            <Alert className="mx-6 my-4 bg-primary/10 border-primary/20 text-primary-foreground">
+                            <Alert className="my-4 bg-primary/10 border-primary/20 text-primary-foreground">
                                 <Gift className="h-5 w-5 text-primary" />
                                 <AlertTitle className="text-primary font-bold">Welcome to the Triple S Canna Club!</AlertTitle>
                                 <AlertDescription className="text-primary/90">
@@ -240,7 +240,7 @@ export const DesignPackDialog: React.FC<DesignPackDialogProps> = ({ isOpen, onOp
                 </DialogContent>
             </Dialog>
             <Dialog open={isImageViewerOpen} onOpenChange={setIsImageViewerOpen}>
-                <DialogContent className="max-w-lg p-2">
+                <DialogContent className="max-w-3xl p-2">
                     <DialogHeader>
                         <DialogTitle>Triple S Canna club design</DialogTitle>
                     </DialogHeader>
